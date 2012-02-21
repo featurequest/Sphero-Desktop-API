bb9cde3945b8972253ece64435ecd108618d86a0
Nicklas Gavelin <nicklas.gavelin@gmail.com>
2012-02-21 15:09:52 +0100

    test

---
+++ src/se/nicklasgavelin/log/testclass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.nicklasgavelin.log;
+
+/**
+ *
+ * @author Nicklas Gavelin, nicklas.gavelin@gmail.com, Luleå University of
+ * Technology
+ */
+public class testclass
+{
+
+}
